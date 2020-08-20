d9e40ab60f88a44d03aef2137a323f38666f1701
Victor Soriano Mendoza <victor.sorianomendoza@gmail.com>
2020-08-19 22:18:21 -0700

    remove pagination bundle, add basic pagination, change track styling to cards from table

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,7 +8,6 @@ use SpotifyWebAPI\SpotifyWebAPI;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Knp\Component\Pager\Paginator;
 
 class UserController extends AbstractController
 {
@@ -57,74 +56,44 @@ class UserController extends AbstractController
         $accessToken = $session->get('accessToken');
         $api->setAccessToken($accessToken);
 
-        $lists = [];
+        $getParams = $request->attributes->get('_route_params');
+        $playlistId = $getParams['playlist_id'];
+
         $next = null;
-        $limit = self::SPOTIFY_WEB_API_SONG_LIMIT;
-        $offset = 0;
         $options = [
-            'limit'     => $limit,
-            'offset'    => $offset
+            'limit'     => self::SPOTIFY_WEB_API_SONG_LIMIT,
+            'offset'    => ($request->attributes->getInt('page', 1) - 1) * self::SPOTIFY_WEB_API_SONG_LIMIT
         ];
 
-        $getParams = $request->attributes->get('_route_params');
-        $playlistId = $getParams['playlistId'];
         $playlistTracks = $api->getPlaylistTracks($playlistId, $options);
 
-        $totalSongs = $playlistTracks->total;
-        $totalPages = ceil($totalSongs / self::SPOTIFY_WEB_API_SONG_LIMIT);
-        $currentPage = ceil(($playlistTracks->offset === 0) ? 1 : $playlistTracks->offset / self::SPOTIFY_WEB_API_SONG_LIMIT);
-
-        $lists = array_merge($lists, $playlistTracks->items);
-        while ($playlistTracks->next) {
-            $next = parse_url($playlistTracks->next);
-            $nextQuery = explode('&', $next['query']);
-            $offset = explode('=', $nextQuery[0])[1];
-            $limit = explode('=', $nextQuery[1])[1];
-            $options = [
-                'limit'     => intval($limit),
-                'offset'    => intval($offset),
-            ];
-            $playlistTracks = $api->getPlaylistTracks($playlistId, $options);
-            $lists = array_merge($lists, $playlistTracks->items);
-        }
-        
-        $tracks = array_map(function ($e) {
-            return $e->track;
-        }, $lists);
-
-        $trackIds = array_map(function ($e) {
-            return $e->id;
-        }, $tracks);
-
-        $trackIdChunks = array_chunk($trackIds, self::SPOTIFY_WEB_API_FEATURE_LIMIT);
+        $tracks = array_column($playlistTracks->items, 'track');
+        $trackIds = array_column($tracks, 'id');
 
         $trackFeatures = [];
-        foreach ($trackIdChunks as $chunk) {
-            $audioFeatures = $api->getAudioFeatures($chunk)->audio_features;
-            foreach ($audioFeatures as $feature) {
+        $audioFeatures = $api->getAudioFeatures($trackIds)->audio_features;
+        foreach ($audioFeatures as $feature) {
+            if (is_object($feature)) {
                 $trackFeatures[$feature->id] = $feature;
             }
         }
 
-        $tracks_with_features = [];
+        $tracksWithFeatures = [];
         foreach ($tracks as $track) {
-            $tracks_with_features[$track->id] = [
-                'track'     =>  $track,
-                'features'  =>  $trackFeatures[$track->id]
-            ];
+            if (array_key_exists($track->id, $trackFeatures)) {
+                $tracksWithFeatures[] = [
+                    'track' => $track,
+                    'features' => $trackFeatures[$track->id]
+                ];
+            }
         }
+        $totalPages = ceil($playlistTracks->total / self::SPOTIFY_WEB_API_SONG_LIMIT);
 
-        $target = range(1, $totalPages);
-        $paginationTemplate = 'playlist_tracks_pagination.html.twig';
-        $paginator = new Paginator();
-        $pagination = $paginator->paginate($target, $currentPage, 10);
-        $pagination->renderer = function($data) use ($paginationTemplate) {
-            return $this->renderView($paginationTemplate, $data);
-        };
-
-        return $this->render('playlist_tracks.html.twig', [
-            'tracks'        =>  $tracks_with_features,
-            'pagination'    =>  $pagination,
+        return $this->render('playlist_tracks_cards.html.twig', [
+            'tracks'                =>  $tracksWithFeatures,
+            'current_page'          =>  $request->attributes->getInt('page', 1),
+            'total_pages'           =>  $totalPages,
+            'playlist_id'           =>  $request->attributes->getAlnum('playlist_id'),
         ]);
     }
 
